293db9d3ce6b994197ab0187cd2e96e1cd8836b9
yoo <edel1212@naver.com>
2026-04-29 17:47:14 +0900

    DTO 필드 내 LocalDateTime이 있을 경우 직렬화 설정

--- redis-project/src/main/java/com/yoo/redis_project/config/RedisConfig.java
+++ redis-project/src/main/java/com/yoo/redis_project/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.yoo.redis_project.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -16,15 +19,32 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+
+        // 🔍 DTO 내 LocalTime 존재 시 "jackson.databind.exc.InvalidDefinitionException" 예외 방지
+        ObjectMapper objectMapper = new ObjectMapper();
+        // ✅ JavaTimeModule 등록
+        objectMapper.registerModule(new JavaTimeModule());
+
+        // ✅ LocalDateTime을 timestamps(숫자) 대신 문자열로 직렬화
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        // 설정이 간단하고 범용적이기에 GenericJackson2JsonRedisSerializer로 직렬화 설정을 한다.
+        GenericJackson2JsonRedisSerializer jsonSerializer =
+                new GenericJackson2JsonRedisSerializer(objectMapper);
+
         // Key 직렬화 (String)
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
+        // 최상위 Key
+        template.setKeySerializer(stringSerializer);
+        // Hash 내부 Field명
+        template.setHashKeySerializer(stringSerializer);
 
         // Value 직렬화 (JSON)
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        // 최상위 Key
+        template.setValueSerializer(jsonSerializer);
+        // Hash 내부 Field명
+        template.setHashValueSerializer(jsonSerializer);
 
-        template.afterPropertiesSet();
         return template;
     }
 }
