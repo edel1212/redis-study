7f91bf3efd0ebaa66f9919dfd0fcc46fa418f676
yoo <edel1212@naver.com>
2026-04-29 16:13:28 +0900

    project init

---
+++ redis-project/src/test/java/com/yoo/redis_project/RedisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoo.redis_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
