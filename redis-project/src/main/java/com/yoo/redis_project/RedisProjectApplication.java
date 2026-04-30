f455a4a295213ee836580ed4c55902544af9fe99
yoo <edel1212@naver.com>
2026-04-30 16:12:28 +0900

    캐싱 사용 - @EnableCaching 활성화

--- redis-project/src/main/java/com/yoo/redis_project/RedisProjectApplication.java
+++ redis-project/src/main/java/com/yoo/redis_project/RedisProjectApplication.java
@@ -2,10 +2,13 @@ package com.yoo.redis_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+// ✅ 캐시 추상화 활성화 - 없을 경우 캐싱 사용 ❌
+@EnableCaching
 public class RedisProjectApplication {
 
 	public static void main(String[] args) {
