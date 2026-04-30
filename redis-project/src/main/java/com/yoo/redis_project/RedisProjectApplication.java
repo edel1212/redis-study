9ffd507f8a5f7a435946aa3934cd7575cb5e4bb9
yoo <edel1212@naver.com>
2026-04-30 13:26:01 +0900

    RDB 사용 세팅

--- redis-project/src/main/java/com/yoo/redis_project/RedisProjectApplication.java
+++ redis-project/src/main/java/com/yoo/redis_project/RedisProjectApplication.java
@@ -2,7 +2,9 @@ package com.yoo.redis_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class RedisProjectApplication {
 
