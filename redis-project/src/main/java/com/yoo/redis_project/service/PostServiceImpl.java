44bc37f68e98cfe18740f724d03d90ccd3a3e577
yoo <edel1212@naver.com>
2026-05-03 21:27:48 +0900

    redisTemplate -> stringRedisTemplate 변경 (직렬화 <-> 역직렬화 이슈)

--- redis-project/src/main/java/com/yoo/redis_project/service/PostServiceImpl.java
+++ redis-project/src/main/java/com/yoo/redis_project/service/PostServiceImpl.java
@@ -4,20 +4,26 @@ import com.yoo.redis_project.dto.PostDto;
 import com.yoo.redis_project.dto.RequestPost;
 import com.yoo.redis_project.entity.PostEntity;
 import com.yoo.redis_project.repository.PostRepository;
+import com.yoo.redis_project.utils.RedisCacheHelper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class PostServiceImpl {
     private final PostRepository postRepository;
-    private final RedisTemplate<String, Object> redisTemplate;
+    // redis String을 Helper class로 사용
+    private final RedisCacheHelper redisCacheHelper;
 
     public void writePost(RequestPost requestPost){
         PostEntity entity = requestPost.toEntity();
@@ -40,21 +46,28 @@ public class PostServiceImpl {
                 .orElseThrow(() -> new RuntimeException("저장된 값을 찾을 수 없습니다."));
     }
 
-    public PostDto getPostWriteLogic(Long id){
+    public PostDto getPostByRedisTemplate(Long id){
         String key = "post:" + id;
         // [cache hit] 캐시 조회
-        PostDto cached = (PostDto) redisTemplate.opsForValue().get(key);
-        if (cached != null) {
+        Optional<PostDto> optionalCached =  redisCacheHelper.get(key, PostDto.class);
+        if (optionalCached.isPresent()) {
+            PostDto cached = optionalCached.get();
             log.info("cache hit : {}", cached);
             return cached;
         } // if
 
         log.info("cache miss read DB");
         // [cache miss] DB 조회
-        return postRepository.findById(id)
+        PostDto postDto = postRepository.findById(id)
                 .map(PostDto::from)
                 .orElseThrow(() -> new RuntimeException("저장된 값을 찾을 수 없습니다."));
+
+        log.info("write cache");
+        redisCacheHelper.set(key, postDto);
+
+        return postDto;
     }
+
 
 
 }
