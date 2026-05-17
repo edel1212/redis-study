2965355136b6ad1cee9f44d03212fa159c8ac9d0
yoo <edel1212@naver.com>
2026-05-17 14:54:23 +0900

    스케줄링 설정 추가

--- redis-project/src/main/java/com/yoo/redis_project/RedisProjectApplication.java
+++ redis-project/src/main/java/com/yoo/redis_project/RedisProjectApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
+// 스케줄링 추가
+@EnableScheduling
+// 추강화 공통 Entity 적용 추가
 @EnableJpaAuditing
-@SpringBootApplication
 // ✅ 캐시 추상화 활성화 - 없을 경우 캐싱 사용 ❌
 @EnableCaching
+@SpringBootApplication
 public class RedisProjectApplication {
 
 	public static void main(String[] args) {
