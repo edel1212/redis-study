fe6189df47fee38413fe3c828b51053864802a61
yoo <edel1212@naver.com>
2026-05-18 16:26:29 +0900

    Resource 예외 추가

--- redis-project/src/main/java/com/yoo/redis_project/exception/custom/ResourceNotFoundException.java
+++ redis-project/src/main/java/com/yoo/redis_project/exception/custom/ResourceNotFoundException.java
@@ -1,9 +1,8 @@
-package com.yoo.redis_project.exception.cusom;
+package com.yoo.redis_project.exception.custom;
 
 /**
  * 요청한 리소스가 존재하지 않을 때 발생하는 예외.
  *
- * @param message 어떤 리소스가 없는지 명시 (예: "콘서트 없음. id=1")
  */
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
